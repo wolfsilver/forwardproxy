416ea5e40872e655702409cb75564da158cc02ec
wolfsilver <wolfsilver@users.noreply.github.com>
2023-08-15 19:48:43 +0800

    test

--- forwardproxy.go
+++ forwardproxy.go
@@ -293,25 +293,25 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyht
 
 		// HTTP CONNECT Fast Open. We merely close the connection if Open fails.
 		wFlusher, ok := w.(http.Flusher)
-		if !ok {
-			return caddyhttp.Error(http.StatusInternalServerError,
-				fmt.Errorf("ResponseWriter doesn't implement http.Flusher"))
+		if ok {
+			// return caddyhttp.Error(http.StatusInternalServerError,
+			// 	fmt.Errorf("ResponseWriter doesn't implement http.Flusher"))
+			// Creates a padding of [30, 30+32)
+			paddingLen := rand.Intn(32) + 30
+			padding := make([]byte, paddingLen)
+			bits := rand.Uint64()
+			for i := 0; i < 16; i++ {
+				// Codes that won't be Huffman coded.
+				padding[i] = "!#$()+<>?@[]^`{}"[bits&15]
+				bits >>= 4
+			}
+			for i := 16; i < paddingLen; i++ {
+				padding[i] = '~'
+			}
+			w.Header().Set("Padding", string(padding))
+			w.WriteHeader(http.StatusOK)
+			wFlusher.Flush()
 		}
-		// Creates a padding of [30, 30+32)
-		paddingLen := rand.Intn(32) + 30
-		padding := make([]byte, paddingLen)
-		bits := rand.Uint64()
-		for i := 0; i < 16; i++ {
-			// Codes that won't be Huffman coded.
-			padding[i] = "!#$()+<>?@[]^`{}"[bits&15]
-			bits >>= 4
-		}
-		for i := 16; i < paddingLen; i++ {
-			padding[i] = '~'
-		}
-		w.Header().Set("Padding", string(padding))
-		w.WriteHeader(http.StatusOK)
-		wFlusher.Flush()
 
 		hostPort := r.URL.Host
 		if hostPort == "" {
